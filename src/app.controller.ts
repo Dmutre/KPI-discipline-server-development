a985ecad42ae1ce1d9d3cbbee3984d23eb209b68
Dmutre <104783173+Dmutre@users.noreply.github.com>
2024-09-25 16:52:31 +0200

    feat(doc): added documentation
    - added documentation for users, so it can be easily tested and checked by users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get, Res } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 
+@ApiTags('App')
 @Controller()
 export class AppController {
-  @Get()
+  @ApiOperation({ summary: 'Health check endpoint' })
+  @ApiOkResponse({ type: String })
+  @Get('/healthcheck')
   getHello(@Res() response: Response) {
     response.status(200).send('Server is working. Status 200 OK response');
   }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get<ConfigService>(ConfigService);
+
+  const options = new DocumentBuilder()
+    .setTitle('API gateway docs')
+    .setDescription('API gateway documentation of camny EHR and B2C platform')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   const PORT = configService.get<number>('port');
   const HOST = configService.get<string>('host');
